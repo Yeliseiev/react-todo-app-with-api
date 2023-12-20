d7568b1e8e5172ba68225aefb869bd2d7df26582
Yelisieiev Kim <rbv271@outlook.com>
2023-12-20 13:30:47 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,24 @@ import React from 'react';
 import logo, { ReactComponent as Logo } from './logo.svg';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.tsx</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Logo />
-    </div>
-  );
-}
-
-export default App;
+export const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit
+        <code>src/App.tsx</code>
+        and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+    <Logo />
+  </div>
+);
